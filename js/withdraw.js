599747d87e7686bd8c4776588bc785f021c304c1
Apumia1 <mia833856@gmail.com>
2022-09-28 18:25:28 +0600

    japan

---
+++ js/withdraw.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    // console.log(newWithdrawAmount);
+    withdrawField.value = '';
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = newWithdrawTotal;
+})
